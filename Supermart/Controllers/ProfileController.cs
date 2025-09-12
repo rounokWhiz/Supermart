c6d8e68b8b0fa05f4058e27f76a7062947fe0eb8
RounokRabbi <rounok107@gmail.com>
2025-09-12 19:33:13 +0600

    implemented ProfileController

---
+++ Supermart/Controllers/ProfileController.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using MyEcommerceAdmin.Models;
+
+namespace MyEcommerceAdmin.Controllers
+{
+    // ProfileController.cs - KEEP USING TemData:
+
+    public class ProfileController : Controller
+    {
+        MyEcommerceDbContext db = new MyEcommerceDbContext();
+
+        // GET: Profile
+        public ActionResult Index()
+        {
+            // Check if admin is logged in using TemData
+            if (TemData.EmpID == 0)
+            {
+                return RedirectToAction("Index", "admin_Login");
+            }
+
+            return View(db.admin_Employee.Find(TemData.EmpID));
+        }
+    }
+}
